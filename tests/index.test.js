f9fcd3d16943d1fbf6375734ffa9ad39b5391731
Antoine Caron <acaron@ineat.fr>
2020-08-04 15:12:15 +0200

    Add throwIf, instanceOf, arrayIf, asArray, assignAsIf, assignmentToPath, executeIfDefined, firstFulfilled Unit testing Patch randomString on empty symbols

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,8 @@
 const { expect } = require('chai');
 require('mocha');
+const { default: Errors } = require('eratum');
 
-const { insertIf, randomString, iterate } = require('../sources/index.ts');
+const { insertIf, randomString, iterate, executeIfDefined, assignmentToPath,firstFulfilled } = require('../sources/index.ts');
 
 describe('Javascript tests', () => {
 	describe('Test insertIf', () => {
@@ -22,6 +23,132 @@ describe('Javascript tests', () => {
 	describe('Test iterate', () => {
 		it('Should return empty array if length is invalid', () => {
 			expect(iterate('srbvr')).deep.equal([]);
+		});
+	});
+
+	describe('Test executeIfDefined', () => {
+		it('Should Throw programing fault if executor is not function', () => {
+			try {
+				const executor = executeIfDefined('Hello world');
+				expect.fail('Should Throw programing fault if executor is not function');
+			} catch (error) {
+				expect(error).instanceOf(Errors.programingFault.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('cause').instanceOf(Errors.invalidType.class);
+				expect(error.cause).property('parameters').a('object');
+				expect(error.cause.parameters).property('name').equal('executor');
+				expect(error.cause.parameters).property('expectedType').equal('function');
+				expect(error.cause.parameters).property('actualType').equal('string');
+			}
+		});
+	});
+
+	describe('Test assignmentToPath', () => {
+		it('Should throw error on function object', () => {
+			try {
+				const obj = assignmentToPath(() => 13, true);
+				expect.fail('Should throw error on function object')
+			} catch (error) {
+				expect(error).instanceOf(Errors.invalidType.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('parameters').a('object');
+				expect(error.parameters).property('name').equal('object');
+				expect(error.parameters).property('expectedType').equal('object');
+				expect(error.parameters).property('actualType').equal('function');
+			}
+		});
+		it('Should throw error on null object', () => {
+			try {
+				const obj = assignmentToPath(null, true);
+				expect.fail('Should throw error on null object')
+			} catch (error) {
+				expect(error).instanceOf(Errors.notEqual.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('parameters').a('object');
+				expect(error.parameters).property('name').equal('object');
+				expect(error.parameters).property('expectedValue').equal('!null');
+				expect(error.parameters).property('actualValue').equal('null');
+			}
+		});
+		it('Should throw error on function path', () => {
+			try {
+				const obj = assignmentToPath({}, () => 'path');
+				expect.fail('Should throw error on function path')
+			} catch (error) {
+				expect(error).instanceOf(Errors.invalidType.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('parameters').a('object');
+				expect(error.parameters).property('name').equal('path');
+				expect(error.parameters).property('expectedType').equal('string');
+				expect(error.parameters).property('actualType').equal('function');
+			}
+		});
+		it('Should throw error on empty path', () => {
+			try {
+				const obj = assignmentToPath({}, '');
+				expect.fail('Should throw error on empty path')
+			} catch (error) {
+				expect(error).instanceOf(Errors.notEqual.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('parameters').a('object');
+				expect(error.parameters).property('name').equal('path');
+				expect(error.parameters).property('expectedValue').equal('not empty');
+				expect(error.parameters).property('actualValue').equal('');
+			}
+		});
+	});
+
+	describe('Test firstFulfilled', () => {
+		it('Should throw error on invalid function fallback', () => {
+			try {
+				firstFulfilled(Promise.resolve(42), null, 42);
+				expect.fail('Should throw error on invalid function fallback')
+			} catch (error) {
+				expect(error).instanceOf(Errors.programingFault.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('cause').instanceOf(Errors.invalid.class);
+				expect(error.cause).property('parameters').a('object');
+				expect(error.cause.parameters).property('name').equal('otherwise');
+
+				expect(error.cause).property('cause').instanceof(Array);
+
+				expect(error.cause.cause[0]).instanceOf(Errors.invalidType.class);
+				expect(error.cause.cause[0]).property('parameters').a('object');
+				expect(error.cause.cause[0].parameters).property('name').equal('otherwise[0]');
+				expect(error.cause.cause[0].parameters).property('actualType').equal('object');
+				expect(error.cause.cause[0].parameters).property('expectedType').equal('function');
+
+				expect(error.cause.cause[1]).instanceOf(Errors.invalidType.class);
+				expect(error.cause.cause[1]).property('parameters').a('object');
+				expect(error.cause.cause[1].parameters).property('name').equal('otherwise[1]');
+				expect(error.cause.cause[1].parameters).property('actualType').equal('number');
+				expect(error.cause.cause[1].parameters).property('expectedType').equal('function');
+			}
+		});
+		it('Should throw error on invalid first promise', () => {
+			try {
+				return firstFulfilled(42, () => null)
+					.then(() => expect.fail('Should throw error on invalid first promise'));
+			} catch (error) {
+				expect(error).instanceOf(Errors.invalidType.class);
+				expect(error).property('origin').equal('TOOLBOX');
+				expect(error).property('parameters').a('object');
+				expect(error.parameters).property('name').equal('promise[0]');
+				expect(error.parameters).property('actualType').equal('Number');
+				expect(error.parameters).property('expectedType').equal('Promise');
+			}
+		});
+		it('Should reject error on invalid fallback promise', () => {
+			return firstFulfilled(Promise.resolve(false), () => 42)
+				.then(() => expect.fail('Should throw error on invalid fallback promise'))
+				.catch((error) => {
+					expect(error).instanceOf(Errors.invalidType.class);
+					expect(error).property('origin').equal('TOOLBOX');
+					expect(error).property('parameters').a('object');
+					expect(error.parameters).property('name').equal('promise[1]');
+					expect(error.parameters).property('actualType').equal('Number');
+					expect(error.parameters).property('expectedType').equal('Promise');
+				});
 		});
 	});
 });
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,10 @@ import { expect } from 'chai';
 import 'mocha';
 import Errors from 'eratum';
 
-import { deepFreeze, insertIf, randomString, SYMOBOLS, iterate } from '../sources';
+import {
+	deepFreeze, insertIf, randomString, SYMOBOLS, iterate,
+	arrayIf, asArray, assignAsIf, executeIfDefined, assignmentToPath, firstFulfilled,
+} from '../sources';
 
 describe('Typescript tests', () => {
 	describe('Test insertIf', () => {
@@ -24,13 +27,13 @@ describe('Typescript tests', () => {
 		it('Should fail if symbol has no length', () => {
 			try {
 				randomString(12, '');
+				expect.fail('Should fail if symbol has no length');
 			} catch (error) {
 				expect(error).instanceOf(Errors.invalid.class);
 				expect(error).property('origin').equal('TOOLBOX');
 				expect(error).property('parameters').a('object');
 				expect(error.parameters).property('name').equal('symbols');
 				expect(error).property('cause').instanceOf(Errors.equal.class);
-				expect(error.cause).property('origin').equal('TOOLBOX');
 				expect(error.cause).property('parameters').a('object');
 				expect(error.cause.parameters).property('name').equal('symbols.length');
 				expect(error.cause.parameters).property('value').equal(0);
@@ -85,6 +88,123 @@ describe('Typescript tests', () => {
 			let length = 0;
 			iterate(size).forEach(() => (length += 1));
 			expect(length).equal(size);
+		});
+	});
+
+	describe('Test arrayIf', () => {
+		it('Should return empty array on falsy', () => {
+			const array = arrayIf(false, 12, 23, 124);
+			expect(array).deep.equal([]);
+		});
+		it('Should return filled array on truly', () => {
+			const input = [ 12, 23, 124 ];
+			const array = arrayIf(true, ...input);
+			expect(array).deep.equal(input);
+		});
+	});
+
+	describe('Test asArray', () => {
+		it('Should return self array', () => {
+			const input = [ 12, 23, 124 ];
+			expect(asArray(input)).equal(input);
+		});
+		it('Should return array containing self', () => {
+			const input = { a: 12 };
+			const array = asArray(input);
+			expect(array.length).equal(1);
+			expect(array[0]).equal(input);
+		});
+	});
+
+	describe('Test assignAsIf', () => {
+		it('Should return object on truly', () => {
+			const key = 'key';
+			const obj = assignAsIf(key, true);
+			expect(obj).instanceof(Object).not.equal(null);
+			expect(obj![key]).equal(true);
+		});
+		it('Should return null on falsy', () => {
+			const key = 'key';
+			expect(assignAsIf(key, false)).equal(null);
+		});
+	});
+
+	describe('Test executeIfDefined', () => {
+		it('Should ignore call on falsy', () => {
+			const executor = executeIfDefined((value: boolean) => {
+				throw new Error(`Should not be called(${value})`);
+			});
+			executor(false);
+		});
+		it('Should forward call on truly', () => {
+			const token = 'TOKEN';
+			let executed = 'false';
+			const executor = executeIfDefined((value: string) => {
+				executed = value;
+			});
+			executor(token);
+			expect(executed).equal(token);
+		});
+	});
+
+	describe('Test assignmentToPath', () => {
+		it('Should return function for assignment', () => {
+			const object = {};
+			const token = 'Hello World';
+			const assignment = assignmentToPath(object, 'key');
+			expect(assignment).a('function');
+			expect(assignment(token)).equal(object)
+				.property('key').equal(token);
+		});
+		it('Should return function for assignment', () => {
+			const object = {};
+			const token = 'Hello World';
+			const assignment = assignmentToPath(object, 'a.b.c');
+			expect(assignment).a('function');
+			expect(assignment(token)).equal(object)
+				.property('a').a('object')
+				.property('b').a('object')
+				.property('c').equal(token);
+		});
+	});
+
+	describe('Test firstFulfilled', () => {
+		it('Should fulfilled value on single promise', () => {
+			return firstFulfilled(Promise.resolve(42))
+				.then((data) => expect(data).equal(42));
+		});
+		it('Should fulfilled first truly value', () => {
+			return firstFulfilled(
+				Promise.resolve(null),
+				() => Promise.resolve(73),
+			).then((data) => expect(data).equal(73));
+		});
+		it('Should not call next promise after fulfillment', () => {
+			let token = 'CONST';
+			return firstFulfilled(
+				Promise.resolve(73),
+				() => new Promise(((resolve) => {
+					token = 'EDITED';
+					resolve();
+				})),
+			).then(() => expect(token).equal('CONST'));
+		});
+		it('Should fulfilled first truly value', () => {
+			const token = 'Hello World';
+			return firstFulfilled<any>(
+				Promise.resolve(false),
+				() => Promise.resolve(null),
+				() => Promise.resolve(''),
+				() => Promise.resolve('Hello World'),
+			).then((data) => expect(data).equal(token));
+		});
+		it('Should fulfilled last falsy value', () => {
+			return firstFulfilled<any>(
+				Promise.resolve(false),
+				() => Promise.resolve(null),
+				() => Promise.resolve(''),
+				() => Promise.resolve(0),
+			).then((data) => expect(data).equal(0));
 		});
 	});
 });
